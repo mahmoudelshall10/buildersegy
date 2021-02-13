e6ac00c03b02a266cd3fcba53edf6dcbaf017340
mahmoud <mahmoudelshall10@outlook.com>
2021-02-13 16:53:28 +0200

    url

--- resources/views/admin/admin_layouts/app.blade.php
+++ resources/views/admin/admin_layouts/app.blade.php
@@ -4,24 +4,24 @@
     <meta charset="UTF-8">
     <title>Builders | Dashboard</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-   <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+   
     <!-- Bootstrap 3.3.2 -->
-    <link href="{{ asset('admin_design/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />    
+    <link href="{{ url('admin_design/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />    
     <!-- FontAwesome 4.3.0 -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <!-- Ionicons 2.0.0 -->
     <link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />    
     <!-- DATA TABLES -->
    
-    <link href="{{ asset('admin_design/plugins/datatables/dataTables.bootstrap.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ url('admin_design/plugins/datatables/dataTables.bootstrap.css') }}" rel="stylesheet" type="text/css" />
     <!-- Theme style -->
    
-    <link href="{{ asset('admin_design/dist/css/AdminLTE.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ url('admin_design/dist/css/AdminLTE.min.css') }}" rel="stylesheet" type="text/css" />
    
     <!-- AdminLTE Skins. Choose a skin from the css/skins 
          folder instead of downloading all of them to reduce the load. -->
-    <link href="{{ asset('admin_design/dist/css/skins/_all-skins.min.css') }}" rel="stylesheet" type="text/css" />
-    <link rel="shortcut icon" href="{{asset('images/builders_5.png')}}" type="image/x-icon">
+    <link href="{{ url('admin_design/dist/css/skins/_all-skins.min.css') }}" rel="stylesheet" type="text/css" />
+    <link rel="shortcut icon" href="{{url('images/builders_5.png')}}" type="image/x-icon">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -42,7 +42,7 @@
             <ul class="nav navbar-nav">
               <li class="dropdown user user-menu">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                <img src="{{asset(Auth::user()->photo)}}" class="user-image" alt="User Image"/>
+                <img src="{{url(Auth::user()->photo)}}" class="user-image" alt="User Image"/>
                   <span class="hidden-xs">
                     {{ Auth::user()->name}}
                   </span>
@@ -50,7 +50,7 @@
                 <ul class="dropdown-menu">
                   <!-- User image -->
                   <li class="user-header">
-                  <a href='{{ asset('admin/users/profile') }}' style="color:rgba(255,255,255,0) !important;background-color:rgba(255,255,255,0) !important;"><img src="{{asset(Auth::user()->photo)}}" style="width:100px;height:100px" class="user-image" alt="User Image" /></a>
+                  <a href='{{ url('admin/users/profile') }}' style="color:rgba(255,255,255,0) !important;background-color:rgba(255,255,255,0) !important;"><img src="{{url(Auth::user()->photo)}}" style="width:100px;height:100px" class="user-image" alt="User Image" /></a>
                     <p>
                       {{ Auth::user()->name}}
                     <small>Member since {{\Carbon\Carbon::parse(Auth::user()->created_at)->format('d M y')}}</small>
@@ -83,7 +83,7 @@
           <!-- Sidebar user panel -->
           <div class="user-panel">
             <div class="pull-left image">
-              <img src="{{asset(Auth::user()->photo)}}" class="img-circle" alt="User Image" />
+              <img src="{{url(Auth::user()->photo)}}" class="img-circle" alt="User Image" />
             </div>
             <div class="pull-left info">
               <p>{{ Auth::user()->name}}</p>
@@ -100,7 +100,7 @@
     </div><!-- ./wrapper -->
 
     <!-- jQuery 2.1.3 -->
-    <script src="{{ asset('admin_design/plugins/jQuery/jQuery-2.1.3.min.js') }}"></script>
+    <script src="{{ url('admin_design/plugins/jQuery/jQuery-2.1.3.min.js') }}"></script>
     <!-- jQuery UI 1.11.2 -->
     <script src="http://code.jquery.com/ui/1.11.2/jquery-ui.min.js" type="text/javascript"></script>
     <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
@@ -110,17 +110,17 @@
     </script>
 -->
     <!-- Bootstrap 3.3.2 JS -->
-    <script src="{{ asset('admin_design/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>    
+    <script src="{{ url('admin_design/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>    
      <!-- DATA TABES SCRIPT -->
-     <script src="{{ asset('admin_design/plugins/datatables/jquery.dataTables.js') }}" type="text/javascript"></script>
-     <script src="{{ asset('admin_design/plugins/datatables/dataTables.bootstrap.js') }}" type="text/javascript"></script>
+     <script src="{{ url('admin_design/plugins/datatables/jquery.dataTables.js') }}" type="text/javascript"></script>
+     <script src="{{ url('admin_design/plugins/datatables/dataTables.bootstrap.js') }}" type="text/javascript"></script>
       
     <!-- Slimscroll -->
-    <script src="{{ asset('admin_design/plugins/slimScroll/jquery.slimscroll.min.js') }}" type="text/javascript"></script>
+    <script src="{{ url('admin_design/plugins/slimScroll/jquery.slimscroll.min.js') }}" type="text/javascript"></script>
     <!-- FastClick -->
-    <script src='{{ asset('admin_design/plugins/fastclick/fastclick.min.js') }}'></script>
+    <script src='{{ url('admin_design/plugins/fastclick/fastclick.min.js') }}'></script>
     <!-- admin_designLTE App -->
-    <script src="{{ asset('admin_design/dist/js/app.min.js') }}" type="text/javascript"></script>
+    <script src="{{ url('admin_design/dist/js/app.min.js') }}" type="text/javascript"></script>
 
     <!-- page script -->
     <script type="text/javascript">
--- resources/views/admin/auth/user_layouts/app.blade.php
+++ resources/views/admin/auth/user_layouts/app.blade.php
@@ -11,27 +11,26 @@
     @endif
 
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-    <link href="{{ asset('admin_design/dist/css/AdminLTE.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ url('admin_design/dist/css/AdminLTE.min.css') }}" rel="stylesheet" type="text/css" />
     <!-- iCheck -->
     <!-- Bootstrap 3.3.2 -->
-    <link href="{{ asset('admin_design/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />    
+    <link href="{{ url('admin_design/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />    
     <!-- FontAwesome 4.3.0 -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
-    <link href="{{ asset('admin_design/plugins/iCheck/square/blue.css') }}" rel="stylesheet" type="text/css" />
- <link rel="shortcut icon" href="{{asset('images/builders_5.png')}}" type="image/x-icon">
+    <link href="{{ url('admin_design/plugins/iCheck/square/blue.css') }}" rel="stylesheet" type="text/css" />
+    <link rel="shortcut icon" href="{{url('images/builders_5.png')}}" type="image/x-icon">
   </head>
 
 @yield('content')
 
-    <script src="{{ asset('admin_design/plugins/jQuery/jQuery-2.1.3.min.js') }}"></script>
+    <script src="{{ url('admin_design/plugins/jQuery/jQuery-2.1.3.min.js') }}"></script>
     <!-- jQuery UI 1.11.2 -->
-    <script src="http://code.jquery.com/ui/1.11.2/jquery-ui.min.js" type="text/javascript"></script>
-    <script src="{{ asset('admin_design/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>    
+    <script src="https://code.jquery.com/ui/1.11.2/jquery-ui.min.js" type="text/javascript"></script>
+    <script src="{{ url('admin_design/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>    
 
     <!-- iCheck -->
-    <script src="{{ asset('admin_design/plugins/iCheck/icheck.min.js') }}" type="text/javascript"></script>
+    <script src="{{ url('admin_design/plugins/iCheck/icheck.min.js') }}" type="text/javascript"></script>
     <script>
       $(function () {
         $('input').iCheck({
