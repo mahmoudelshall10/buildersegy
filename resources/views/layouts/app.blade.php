5fdb7bf8876fdaecc0366d45263e4617bbb38394
mahmoud <mahmoudelshall10@outlook.com>
2021-02-13 02:56:14 +0200

    asset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,29 +12,29 @@
     <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v1.2.1/mapbox-gl.css' rel='stylesheet' />
     <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v1.2.1/mapbox-gl.js'></script>
     @if(Request::segment(1) == 'bspyl-home' || Request::segment(1) == 'bspyl-about' || Request::segment(1) == 'bspyl-stages' ||Request::segment(1) == 'bspyl-form' )
-    <link rel="shortcut icon" href="{{asset('images/bspyl.png')}}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{secure_asset('images/bspyl.png')}}" type="image/x-icon">
     @else
-    <link rel="shortcut icon" href="{{asset('images/builders_5.png')}}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{secure_asset('images/builders_5.png')}}" type="image/x-icon">
     @endif
    
-    <link rel="stylesheet" href="{{ asset('css/open-iconic-bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/animate.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/open-iconic-bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/animate.css') }}">
     
-    <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/magnific-popup.css') }}">
 
-    <link rel="stylesheet" href="{{ asset('css/aos.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/aos.css') }}">
 
-    <link rel="stylesheet" href="{{ asset('css/ionicons.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/ionicons.min.css') }}">
 
-    <link rel="stylesheet" href="{{ asset('css/bootstrap-datepicker.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/jquery.timepicker.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/bootstrap-datepicker.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/jquery.timepicker.css') }}">
 
     
-    <link rel="stylesheet" href="{{ asset('css/flaticon.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/icomoon.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/flaticon.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/icomoon.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
 
   </head>
   <body>
@@ -45,11 +45,11 @@
     <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
       <div class="container">
         
-        <a class="navbar-brand" href="{{ asset('index')}}">
+        <a class="navbar-brand" href="{{ secure_asset('index')}}">
         @if(Request::segment(1) == 'bspyl-home' || Request::segment(1) == 'bspyl-about' || Request::segment(1) == 'bspyl-stages' ||Request::segment(1) == 'bspyl-form' )
-        <img src='{{asset('images/bspyl.png')}}' style="height:50px;">
+        <img src='{{secure_asset('images/bspyl.png')}}' style="height:50px;">
         @else
-        <img src='{{asset('images/builders_5.png')}}' height='100px'>
+        <img src='{{secure_asset('images/builders_5.png')}}' height='100px'>
         @endif
         </a>
         
@@ -60,9 +60,9 @@
         <div class="collapse navbar-collapse" id="ftco-nav">
           <ul class="navbar-nav ml-auto">
     
-            <li class="{{ Request::segment(1) === 'index' ? 'nav-item active' : 'nav-item' }}"><a href="{{ asset('index')}}" class="nav-link">Home</a></li>
+            <li class="{{ Request::segment(1) === 'index' ? 'nav-item active' : 'nav-item' }}"><a href="{{ secure_asset('index')}}" class="nav-link">Home</a></li>
 
-            <li class="{{ Request::segment(1) === 'about' ? 'nav-item active' : 'nav-item' }}"><a href="{{ asset('about') }}" class="nav-link">About Us</a></li>
+            <li class="{{ Request::segment(1) === 'about' ? 'nav-item active' : 'nav-item' }}"><a href="{{ secure_asset('about') }}" class="nav-link">About Us</a></li>
 
             <li class="nav-item dropdown" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><a class="nav-link">Services</a>
             
@@ -82,11 +82,11 @@
             
             </li>
 
-            <li class="{{ Request::segment(1) === 'builders-story' ? 'nav-item active' : 'nav-item' }}"><a href="{{ asset('builders-story') }}" class="nav-link">Builders Story</a></li>
+            <li class="{{ Request::segment(1) === 'builders-story' ? 'nav-item active' : 'nav-item' }}"><a href="{{ secure_asset('builders-story') }}" class="nav-link">Builders Story</a></li>
 
-            <li class="{{ Request::segment(1) === 'contact' ? 'nav-item active' : 'nav-item' }}"><a href="{{ asset('contact') }}" class="nav-link">Contact</a></li>
+            <li class="{{ Request::segment(1) === 'contact' ? 'nav-item active' : 'nav-item' }}"><a href="{{ secure_asset('contact') }}" class="nav-link">Contact</a></li>
             
-            <li><a class="btn btn-danger" href='{{ asset('bspyl-home') }}' role="button" class="nav-link" style="margin-top: 8px;">Join BSPYL</a></li>
+            <li><a class="btn btn-danger" href='{{ secure_asset('bspyl-home') }}' role="button" class="nav-link" style="margin-top: 8px;">Join BSPYL</a></li>
           </ul>
         </div>
         
@@ -98,17 +98,17 @@
     @if(Request::segment(1) != 'index' )
 
       @if(Request::segment(1) == 'contact')
-      <section class="hero-wrap hero-wrap-2" style="background-image: url({{ asset('images/contact_us.jpg') }} );" data-stellar-background-ratio="0.5">
+      <section class="hero-wrap hero-wrap-2" style="background-image: url({{ secure_asset('images/contact_us.jpg') }} );" data-stellar-background-ratio="0.5">
       <div class="overlay"></div>
       @elseif(Request::segment(1) == 'builders-story-single')
-      <section class="hero-wrap hero-wrap-2" style="background-image: url( {{ asset('images/business_3.jpg') }} );" data-stellar-background-ratio="0.5">
+      <section class="hero-wrap hero-wrap-2" style="background-image: url( {{ secure_asset('images/business_3.jpg') }} );" data-stellar-background-ratio="0.5">
       <div class="overlay"></div>
       @elseif(Request::segment(1) == 'bspyl-home' || Request::segment(1) == 'bspyl-about' || Request::segment(1) == 'bspyl-stages' ||Request::segment(1) == 'bspyl-form' )
-      <section class="hero-wrap hero-wrap-2" style="background-image: url( {{ asset('images/image_4.jpg') }} );" data-stellar-background-ratio="0.5">
+      <section class="hero-wrap hero-wrap-2" style="background-image: url( {{ secure_asset('images/image_4.jpg') }} );" data-stellar-background-ratio="0.5">
       <div class="overlay"></div>
       @else
 
-      <section class="hero-wrap hero-wrap-2" style="background-image: url( {{asset('images/business.jpg')}} );" data-stellar-background-ratio="0.5">
+      <section class="hero-wrap hero-wrap-2" style="background-image: url( {{secure_asset('images/business.jpg')}} );" data-stellar-background-ratio="0.5">
       <div class="overlay"></div>
       @endif
 
@@ -140,7 +140,7 @@
                 <h1 class="mb-3 bread">Builders Simulation Program</h1>
               @endif
               
-              <p class="breadcrumbs"><span class="mr-2"><a href="{{ asset('index')}}">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>
+              <p class="breadcrumbs"><span class="mr-2"><a href="{{ secure_asset('index')}}">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>
                 @if(Request::segment(1) === 'about')
                 About Us
                 @elseif(Request::segment(1) === 'consultancy')
@@ -165,7 +165,7 @@
         </div>
     </section>
   @else
-  <div class="hero-wrap js-fullheight" style="background-image: url({{asset('images/consultation.jpg')}});" data-stellar-background-ratio="0.5">
+  <div class="hero-wrap js-fullheight" style="background-image: url({{secure_asset('images/consultation.jpg')}});" data-stellar-background-ratio="0.5">
       <div class="overlay"></div>
       <div class="container">
         <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center" data-scrollax-parent="true">
@@ -180,7 +180,7 @@
     @endif
 
     @else
-    <div class="hero-wrap js-fullheight" style="background-image: url({{asset('images/consultation.jpg')}});" data-stellar-background-ratio="0.5">
+    <div class="hero-wrap js-fullheight" style="background-image: url({{secure_asset('images/consultation.jpg')}});" data-stellar-background-ratio="0.5">
       <div class="overlay"></div>
       <div class="container">
         <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center" data-scrollax-parent="true">
@@ -265,21 +265,21 @@
     </svg>
   </div>
 <!-- About -->
-  <script src="{{ asset('js/jquery.min.js') }}"></script>
+  <script src="{{ secure_asset('js/jquery.min.js') }}"></script>
   <script src="https://kit.fontawesome.com/89967b7876.js"></script>
-  <script src="{{ asset('js/jquery-migrate-3.0.1.min.js') }}"></script>
-  <script src="{{ asset('js/popper.min.js') }}"></script>
-  <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-  <script src="{{ asset('js/jquery.easing.1.3.js') }}"></script>
-  <script src="{{ asset('js/jquery.waypoints.min.js') }}"></script>
-  <script src="{{ asset('js/jquery.stellar.min.js') }}"></script>
-  <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
-  <script src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
-  <script src="{{ asset('js/aos.js') }}"></script>
-  <script src="{{ asset('js/jquery.animateNumber.min.js') }}"></script>
-  <script src="{{ asset('js/bootstrap-datepicker.js') }}"></script>
-  <script src="{{ asset('js/scrollax.min.js') }}"></script>
-  <script src="{{ asset('js/main.js') }}"></script>
+  <script src="{{ secure_asset('js/jquery-migrate-3.0.1.min.js') }}"></script>
+  <script src="{{ secure_asset('js/popper.min.js') }}"></script>
+  <script src="{{ secure_asset('js/bootstrap.min.js') }}"></script>
+  <script src="{{ secure_asset('js/jquery.easing.1.3.js') }}"></script>
+  <script src="{{ secure_asset('js/jquery.waypoints.min.js') }}"></script>
+  <script src="{{ secure_asset('js/jquery.stellar.min.js') }}"></script>
+  <script src="{{ secure_asset('js/owl.carousel.min.js') }}"></script>
+  <script src="{{ secure_asset('js/jquery.magnific-popup.min.js') }}"></script>
+  <script src="{{ secure_asset('js/aos.js') }}"></script>
+  <script src="{{ secure_asset('js/jquery.animateNumber.min.js') }}"></script>
+  <script src="{{ secure_asset('js/bootstrap-datepicker.js') }}"></script>
+  <script src="{{ secure_asset('js/scrollax.min.js') }}"></script>
+  <script src="{{ secure_asset('js/main.js') }}"></script>
 
 
 <script>
