56aba461f1f4d44df67a1c11913c6e610cd9245c
mahmoud <mahmoudelshall10@outlook.com>
2021-02-13 03:09:06 +0200

    provider-asset

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,8 +14,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if(env('APP_ENV') === 'production') {
-            \URL::forceScheme('https');
+        if(env('APP_ENV') !== 'local') 
+        {
+            $this->app['request']->server->set('HTTPS', true);
         }
     }
 
